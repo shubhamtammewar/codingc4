e97ee7c1b58ffc3a571b68d9e76dbb4c01201c5f
shubhamtammewar <91666990+shubhamtammewar@users.noreply.github.com>
2021-12-13 19:02:56 +0530

    Create show.model.js

---
+++ coding-unit-4-c4/src/models/show.model.js
@@ -0,0 +1,13 @@
+const {Schema,model}=require('mongoose');
+const showSchema=new Schema({
+    timing:{type:String,required:true},
+    movie:{type:Schema.Types.ObjectId,ref:"movie",required:true},
+    total_seats:{type:Number,required:true},
+    screen:{type:Schema.Types.ObjectId,ref:"screen",required:true}
+},
+    {
+        versionKey:false,
+        timestamp:true,
+    }
+)
+module.exports =model("show",showSchema)
